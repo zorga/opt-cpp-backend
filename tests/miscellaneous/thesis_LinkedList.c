121a5b0609a16ba97560ef0f86ce6baf2d74ceeb
zorga <zorganico@gmail.com>
2016-05-07 22:58:39 +0200

    using codemirror to display the student code

--- tests/miscellaneous/thesis_LinkedList.c
+++ tests/miscellaneous/thesis_LinkedList.c
@@ -4,13 +4,13 @@
 
 int main (int argc, char** argv)
 {
-	//node_t* head = init (18);
+  //node_t* head = init (18);
   node_t* head = malloc (sizeof (node_t));
   head->data = 18;
   head->next = NULL;
-	push (&head, 17);
-	free_list (&head);
-	return 0;
+  push (&head, 17);
+  free_list (&head);
+  return 0;
 }
 
 void RemoveDuplicates (node_t* head)
